ed25a46279a5dd51594f69a9a9dfd7b478f51397
Romima6 <romimmals14@gmail.com>
2024-06-14 10:35:07 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const chalkAnimation = require('chalkercli');
+let str = String.raw`
+𝙰-6𝚈 𝙻𝙾𝙰𝙳𝙸𝙽𝙶 𝚆𝙰𝙸𝚃 𝙿𝙻𝙴𝙰𝚂𝙴[▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒] 
+`;
+let logo = String.raw`𝙰-6𝚈 𝙻𝙾𝙰𝙳𝙸𝙽𝙶 𝙰𝙿𝙸 𝚂𝚃𝙰𝚁𝚃𝙸𝙽𝙶 𝙿𝙻𝙴𝙰𝚂𝙴 𝚆𝙰𝙸𝚃..!`;
+                             
+const karaoke = chalkAnimation.karaoke(str);
+const rainbow = chalkAnimation.rainbow(logo);
+setTimeout(async() => {
+    await karaoke.start()
+    await rainbow.start()
+    console.clear()
+}, 1000);
+
+setTimeout(() => {
+    karaoke.stop()
+    rainbow.stop()
+    require('./app/main.js')
+}, 7000);
